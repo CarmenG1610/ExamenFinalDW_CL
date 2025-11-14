336aef4e7b3b07e59c09ff2f16035a53d2410b24
CarmenG1610 <mlopezg114@miumg.edu.gt>
2025-11-13 19:00:41 -0800

    Add files via upload

---
+++ AppEFinal/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Categoria, Producto
+from .forms import CategoriaForm, ProductoForm
+
+# --------------------------
+# CATEGORÍAS - CRUD
+# --------------------------
+
+def lista_categorias(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'categorias/lista.html', {'categorias': categorias})
+
+
+def crear_categoria(request):
+    form = CategoriaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('lista_categorias')
+    return render(request, 'categorias/form.html', {'form': form})
+
+
+def editar_categoria(request, id):
+    categoria = get_object_or_404(Categoria, id=id)
+    form = CategoriaForm(request.POST or None, instance=categoria)
+    if form.is_valid():
+        form.save()
+        return redirect('lista_categorias')
+    return render(request, 'categorias/form.html', {'form': form})
+
+
+def eliminar_categoria(request, id):
+    categoria = get_object_or_404(Categoria, id=id)
+    categoria.delete()
+    return redirect('lista_categorias')
+
+
+# --------------------------
+# PRODUCTOS - CRUD
+# --------------------------
+# LISTADO
+def lista_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'productos/lista.html', {'productos': productos})
+
+# CREAR
+def crear_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('productos_listado')
+    else:
+        form = ProductoForm()
+    return render(request, 'productos/form.html', {'form': form})
+
+# EDITAR
+def editar_producto(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect('productos_listado')
+    else:
+        form = ProductoForm(instance=producto)
+    return render(request, 'productos/form.html', {'form': form})
+
+# ELIMINAR
+def eliminar_producto(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    producto.delete()
+    return redirect('productos_listado')
